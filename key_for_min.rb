4814a6de906d809894886dd1b465e2b518aeca31
Kevin Huang <khuang159@gmail.com>
2020-09-17 15:16:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
-# def key_for_min_value(name_hash)
+def key_for_min_value(name_hash)
 
-# name_hash.each do |key, value|
+name_hash.each do |key, value|
  
-# end
-# end
+end
+end
 
  puts hash[0]
  
