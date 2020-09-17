9903ed9287e8002a2eaa769554603cf9391a995c
Kevin Huang <khuang159@gmail.com>
2020-09-17 14:42:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  total = name_hash[0][0]
+  puts name_hash[0][0]
 key_for_min_value.each do |key, value|
   
 end
