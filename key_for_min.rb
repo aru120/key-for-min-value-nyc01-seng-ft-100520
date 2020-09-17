6d21f832d7e232df2bb4767ab5e8ee2fff62976e
Kevin Huang <khuang159@gmail.com>
2020-09-17 15:48:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 
 def key_for_min_value(name_hash)
-lowest = 
-hashkey = 
+lowest = nil
+hashkey = nil
 name_hash.each do |key, value|
-  if value < lowest
+  if value == nil || value < lowest
    lowest = value
    hashkey = key
 end
